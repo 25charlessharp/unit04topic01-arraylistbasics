61bf17048ad99ea7e77055701a307ac9aad68a97
Charles Sharp <25charlessharp@usd266.com>
2023-03-29 12:04:46 -0500

    almost done with without long words

--- ArrayListUtilities.java
+++ ArrayListUtilities.java
@@ -2,6 +2,21 @@ import java.util.ArrayList;
 
 public class ArrayListUtilities {
     public static int getListMax(ArrayList<Integer> items) {
-        return 0;
+        int maxNum = 0;
+        for(int num : items){
+            if(num > maxNum){
+                maxNum = num;
+            }
+        }
+        return maxNum;
+    }
+
+    public static ArrayList<String> withoutLongWords(ArrayList<String> longWords){
+        ArrayList<String> shortWords = new ArrayList<String>;
+        for(int i; i < longWords.size()-1; i ++){
+            if(longWords.get(i).length() <= 5){
+                shortWords.add(longWords(i));
+            }
+        }
     }
 }
